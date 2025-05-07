c2d670961bdfa5009d620ea81e840eda1a3d88f7
Ayub Ahmed <161202637+ayub56@users.noreply.github.com>
2025-05-07 15:34:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 num1=0
 num2=0
+num3=0
+num4=0
 sum1=input("enter a number:")
 sum2=input("enter second number:")
+sum3=input("enter third number:")
+sum4=input("enter four number:")
 reult=input("enter the operator: *,/,+,-")
 if reult=="+":
     num1=int(sum1)
     num2=int(sum2)
-    print(num1+num2)
+    num3=int(sum3)
+    num4=int(sum4)
+    print(num1+num2+num3+num4)
 if reult=="-":
     num1=int(sum1)
     num2=int(sum2)
-    print(num1-num2)
+    num3=int(sum3)
+    num4=int(sum4)
+    print(num1-num2-num3-num4)
 if reult=="*":
     num1=int(sum1)
     num2=int(sum2)
-    print(num1*num2)
+    num3=int(sum3)
+    num4=int(sum4) 
+    print(num1*num2*num3*num4)
 
 try:
     if reult=="/":
       num1=int(sum1)
       num2=int(sum2)
+      num3=int(sum3)
+      num4=int(sum4)   
       division=num1/num2
       print(division)
 except  ZeroDivisionError :
